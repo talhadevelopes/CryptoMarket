ef41d808f225e83583968f1918b9418437ff32a3
Talha Ahmed <talha.codes970@gmail.com>
2025-03-22 23:38:16 +0530

    updates

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-  mode: 'jit', // Enable JIT compiler
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JS/TS/JSX/TSX files in src
+    "./public/index.html", // Include your HTML file
+  ],
   theme: {
     extend: {
       spacing: {
-        '200': '200px',
-        "30": "30px",
-        "50": "50px",
-        // Add custom spacing if needed
+        200: "200px", // Custom spacing
+        30: "30px", // Custom spacing
+        50: "50px", // Custom spacing
+        // Add more custom spacing if needed
       },
     },
   },
+  plugins: [], // Add Tailwind plugins here if needed
 };
