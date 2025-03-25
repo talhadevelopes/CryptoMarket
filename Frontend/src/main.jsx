c41de09e3558e8e84c8d0ebaef920d68a179f724
Talha Ahmed <talha.codes970@gmail.com>
2025-03-25 17:11:01 +0530

    deployment

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import App from "./App.jsx";
 import store from "./redux/store.js";
 import { BrowserRouter as Router } from "react-router-dom";
-import "antd/dist/reset.css"; // This is the updated import for Ant Design 5.x
+import "antd/dist/reset.css"; 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
